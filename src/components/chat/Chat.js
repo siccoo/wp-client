13e8b248d7f089f61e35b8c095ea3d1b67c4ec7a
Michael Chilaka <obichilaka@gmail.com>
2022-02-28 12:53:49 +0100

    updates on Chat components

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Avatar, IconButton } from '@material-ui/core';
-import {AttachFile, MoreVert, SearchOutlined} from "@material-ui/icons";
+import { AttachFile, MoreVert, SearchOutlined, InsertEmoticon, Mic } from "@material-ui/icons";
 
 import "./Chat.css";
 
@@ -53,7 +53,14 @@ const Chat = () => {
                     </span>
                 </p>
             </div>
-            <div className='chat__footer'></div>
+            <div className='chat__footer'>
+                <InsertEmoticon />
+                <form>
+                    <input type="text" placeholder='Type a message' />
+                    <button type='submit'>Send a message</button>
+                </form>
+                <Mic />
+            </div>
         </div>
     )
 }
